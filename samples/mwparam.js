5d1cb4138044f700e89f8202037174e4e8b391e0
hsl0 <46296273+hsl0@users.noreply.github.com>
2019-11-25 01:51:33 +0900

    요소 시작 간소화

--- samples/mwparam.js
+++ samples/mwparam.js
@@ -1,38 +1,47 @@
 (function() {
+    /**
+     * 파라미터
+     * {{{(키)|(기본값)}}}
+     * @class
+     * @param {(string|Content)} [key] - 키 
+     * @param {(string|Content)} [def] - 기본값
+     * @property {(string|Content)} key - 키
+     * @property {(string|Content)} [def] - 기본값
+     */
     function Parameter(key, def) {
         this.key = key || null;
         this.default = def || null;
     }
+    /**
+     * 문자열로 변환
+     * @method
+     * @returns {string}
+     */
     Parameter.prototype.toString = function() {
         return '{{{' + this.key + (this.default ? '|' + this.default : '') + '}}}';
     }
+    /**
+     * 파라미터 문법에 사용되는 토큰
+     */
     Parameter.tokens = new Map([
         ['|', function(c) {
-            var stack = c.parent.stack;
-            stack.push(c.take());
-            if (!stack.current.key) stack.current.key = stack.done();
-            stack.begin();
-            c.find(Parameter.tokens);
+            var stack = c.parent.stack; // 스택 저장소
+            stack.push(c.take()); // 본문 수집
+            if(!stack.current.key) stack.current.key = stack.done(); // 키가 없으면 기존 계층 종료 및 키에 저장
+            stack.begin(); // 새 계층 시작
+            c.find(Parameter.tokens); // 다음 토큰 검색
         }],
         ['}}}', function(c) {
-            var stack = c.parent.stack;
-            stack.push(c.take());
-            if (stack.current.key) stack.current.default = stack.done();
-            else stack.current.key = stack.done();
-            c.find(c.parent.parent.tokens);
+            var stack = c.parent.stack; // 스택 저장소
+            stack.push(c.take()); // 본문 수집
+            if (stack.current.key) stack.current.default = stack.done(); // 키가 있으면 기본값으로 저장
+            else stack.current.key = stack.done(); // 키가 없으면 키로 저장
+            c.find(c.parent.parent.tokens); // 다른 요소 토큰 검색
         }]
     ]);
-    return new parsorama.Parser({
-        Parameter
-    }, new Map([
+    return new parsorama.Parser({Parameter}, new Map([
         ['{{{', function(c) {
-            var stack = c.parent.stack;
-            stack.push(c.take());
-            stack.push(new Parameter());
-            stack.begin();
-            c.find(Parameter.tokens);
+            c.begin(Parameter);
         }]
-    ]), function(c) {
-        c.find(c.parent.tokens);
-    });
+    ])
 })();
